d9b5082fe060dfbef25991aaa668796ed5abf154
Blaine Bublitz <blaine.bublitz@gmail.com>
2021-04-13 23:46:25 -0700

    feat: Support passing filenames in options for each output type
    This adds `cSourceFileName`, `dataFileName`, `wasmFileName`, and
    `watFileName` to the Compiler options. It also moves the fastfile
    creation into the Compiler itself (instead of the CLI) and will
    call fastfile only if the new *FileName options are passed.

--- ports/wasm/tester.js
+++ ports/wasm/tester.js
@@ -9,7 +9,6 @@ const compiler = require("../../src/compiler");
 const utils = require("../../src/utils");
 const loadR1cs = require("r1csfile").load;
 const ZqField = require("ffjavascript").ZqField;
-const fastFile = require("fastfile");
 
 const WitnessCalculatorBuilder = require("circom_runtime").WitnessCalculatorBuilder;
 
@@ -25,14 +24,12 @@ async function  wasm_tester(circomFile, _options) {
     const baseName = path.basename(circomFile, ".circom");
     const options = Object.assign({}, _options);
 
-    options.wasmFile = await fastFile.createOverride(path.join(dir.path, baseName + ".wasm"));
+    options.wasmFileName = path.join(dir.path, baseName + ".wasm");
 
     options.symWriteStream = fs.createWriteStream(path.join(dir.path, baseName + ".sym"));
     options.r1csFileName = path.join(dir.path, baseName + ".r1cs");
 
     await compiler(circomFile, options);
-
-    await options.wasmFile.close();
 
     const wasm = await fs.promises.readFile(path.join(dir.path, baseName + ".wasm"));
 
